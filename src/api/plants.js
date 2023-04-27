5fb098a466094fa047d2c41338b072ad4bce3860
Damien Pellerin <damienpellerin19@gmail.com>
2023-04-27 10:21:00 +0200

    react router

--- src/api/plants.js
+++ src/api/plants.js
@@ -4,9 +4,11 @@ import { BASE_URL, API_KEY } from "../config";
 export class PlantsAPI {
   static async fetchSpecies() {
     const response = await axios.get(
-      `${BASE_URL}species?token=${API_KEY}&language=fr`
+      `${BASE_URL}plants?token=${API_KEY}&language=fr`
     );
     console.log(response.data);
     return response.data;
   }
+
+  // recupération des plantes par especes
 }
